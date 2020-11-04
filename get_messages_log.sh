a1d0b4d419701f0e76e8386af0f8adca82d3cd1d
Bob <bob@bob.com>
2020-11-03 21:00:39 -0600

    add output file

--- get_messages_log.sh
+++ get_messages_log.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+
 #name=$1
-echo $HOSTNAME
-date
-echo -e "\nThis is bash script"
-echo -e "Hello ${name}\n\n"
-echo "Hello ${name}"
+echo $HOSTNAME | tee -a log_file
+date | tee -a log_file
+echo -e "\nThis is bash script" | tee -a log_file
+echo -e "Hello ${name}\n\n" | tee -a log_file
+echo "Hello ${name}" | tee -a log_file
